250ad9dee0ab2efb4cc89c29672d7449a71ffa63
nico <the.vegeta@gmail.com>
2015-12-27 22:32:01 +0100

    First POC.

--- app/src/main/java/com/dev/nbonnec/mediaseb/MainActivity.java
+++ app/src/main/java/com/dev/nbonnec/mediaseb/MainActivity.java
@@ -1,13 +1,17 @@
 package com.dev.nbonnec.mediaseb;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
-import android.text.method.ScrollingMovementMethod;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.squareup.okhttp.FormEncodingBuilder;
 import com.squareup.okhttp.OkHttpClient;
@@ -23,23 +27,20 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
     private final OkHttpClient client = new OkHttpClient();
     private final static String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36";
-    private ListView list;
     private Document doc = null;
-    private Elements titles;
     private List<String> list_title = new ArrayList<String>();
 
-    public String run() throws Exception {
+    public String run(String search) throws Exception {
         RequestBody formBody = new FormEncodingBuilder()
                 .add("SMMaster", "SMMaster|BtRechSimple")
                 .add("__EVENTTARGET", "")
                 .add("__EVENTARGUMENT", "")
                 .add("__VIEWSTATE", "")
-                .add("TBRechLibre", "walking dead")
+                .add("TBRechLibre", search)
                 .add("CtrlOpacCompte$TBCompteNom", "")
                 .add("CtrlOpacCompte$TBCompteCarte", "")
                 .add("TBrechLivreMobile", "")
@@ -66,7 +67,6 @@ public class MainActivity extends AppCompatActivity {
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        String answ = null;
         super.onCreate(savedInstanceState);
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
@@ -75,20 +75,53 @@ public class MainActivity extends AppCompatActivity {
 
         client.networkInterceptors().add(new UserAgentInterceptor(USER_AGENT));
 
-        list = (ListView) findViewById(R.id.list);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list_title);
+        handleIntent(getIntent());
+    }
 
-        try {
-            doc = Jsoup.parse(this.run());
-        } catch (Exception e){
-            Log.e("MEDIASEB", "exception", e);
-        }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
 
-        if (doc != null) {
-            titles = doc.select("div[class=\"fll span8\"] > a[title]");
-            for (Element element : titles)
-                list_title.add(element.ownText());
-            list.setAdapter(adapter);
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        ListView list;
+        Elements titles;
+
+        Log.i("MEDIASEB", "pass");
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+
+            list = (ListView) findViewById(R.id.list);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list_title);
+
+            try {
+                doc = Jsoup.parse(this.run(query));
+            } catch (Exception e){
+                Log.e("MEDIASEB", "exception", e);
+            }
+
+            if (doc != null) {
+                titles = doc.select("div[class=\"fll span8\"] > a[title]");
+                for (Element element : titles)
+                    list_title.add(element.ownText());
+                list.setAdapter(adapter);
+            }
         }
     }
+
 }
