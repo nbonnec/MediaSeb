9e533bd8c8d35d61a3fee2a861e67633c87c5fdd
thevegeta <the.vegeta@gmail.com>
2016-03-05 12:01:05 +0100

    Use new Toolbar.

--- app/src/main/java/com/nbonnec/mediaseb/ui/activity/MainActivity.java
+++ app/src/main/java/com/nbonnec/mediaseb/ui/activity/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 
@@ -29,7 +30,12 @@ public class MainActivity extends AppCompatActivity {
         // Inject dependencies
         MediasebApp app = MediasebApp.get(getApplicationContext());
         app.inject(this);
+
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
 
         if (findViewById(R.id.container) != null) {
             if (savedInstanceState != null) {
